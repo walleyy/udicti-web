b688371a74e07c6ab682c87d6d0e300e7126f1c6
walte <walterconrad.wc@gmail.com>
2020-07-21 09:39:39 +0300

    admin and coach pages

---
+++ src/app/coach/student-activity/student-activity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-activity',
+  templateUrl: './student-activity.component.html',
+  styleUrls: ['./student-activity.component.scss']
+})
+export class StudentActivityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
