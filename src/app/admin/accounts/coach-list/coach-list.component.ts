bbf8ffcaa3bc4896d9e9fe8799f827301f56a883
walte <walterconrad.wc@gmail.com>
2020-08-07 02:03:30 +0300

    admin

--- src/app/admin/accounts/coach-list/coach-list.component.ts
+++ src/app/admin/accounts/coach-list/coach-list.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-coach-list',
@@ -6,8 +16,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coach-list.component.scss']
 })
 export class CoachListComponent implements OnInit {
+  constructor(
+  ) { }
+  expertise: string[] = ['startupExp', 'accelerationExp', 'marketExp'];
+  image: ImageData;
 
-  constructor() { }
+  @ViewChild('imageUpload', {static: false}) imageUpload: ElementRef; images  = [];
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  matcher = new MyErrorStateMatcher();
+  typeFormcontrol = new FormControl([
+    Validators.required,
+  ]);
+
+  handleImage(event: any) {
+    this.image = event.target.images[0];
+    console.log(this.image);
+  }
 
   ngOnInit() {
   }
